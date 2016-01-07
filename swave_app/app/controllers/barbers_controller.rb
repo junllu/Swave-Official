3c8a55b60d953eafe018f928123c4244c2b12fd3
Richard Ho <honeypotpanda@gmail.com>
2016-01-07 13:46:23 -0800

    Add create new barber

--- swave_app/app/controllers/barbers_controller.rb
+++ swave_app/app/controllers/barbers_controller.rb
@@ -1,2 +1,19 @@
 class BarbersController < ApplicationController
+  def show
+    @barber = Barber.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @barber = Barber.new(barber_params)
+    @barber.save
+    redirect_to @barber
+  end
+
+  private
+    def barber_params
+      params.require(:barber).permit(:username, :name, :personal_bio);
+    end
 end
