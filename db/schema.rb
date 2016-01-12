f2678cf9a8b41679dedc0896b638513c693ad8f0
Richard Ho <honeypotpanda@gmail.com>
2016-01-12 13:04:27 -0800

    Add email uniqueness at the database level

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112050618) do
+ActiveRecord::Schema.define(version: 20160112210029) do
 
   create_table "barbers", force: :cascade do |t|
     t.string   "username"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20160112050618) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
