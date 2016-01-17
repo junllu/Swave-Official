ba5dc98c985b7495f8ca17decf5c19ca76967e3c
junllu <jllu@ucsd.edu>
2016-01-16 17:55:26 -0800

    fixed nav button position

--- public/js/init.js
+++ public/js/init.js
@@ -1,13 +1,14 @@
 (function($){
-  $(function(){
+ $(function(){
 
-    $('.button-collapse').sideNav();
+   $('.button-collapse').sideNav();
+   $('.parallax').parallax();
 
-  }); // end of document ready
+ }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+ $(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal();
+ });
          
