f88a38cf53c52f56efeba4b8b4a084b3c119a3b0
junllu <jllu@ucsd.edu>
2016-02-11 21:52:45 -0800

    updated init file

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -18,4 +18,12 @@ $(document).ready(function(){
   responsiveFallback: false,
   loop: false
   });
+
+    $(document).ready(function() {
+    $('select').material_select();
+  });
+    
+      $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
 });
